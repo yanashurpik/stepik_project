5f11444792d295320132911f67cf28a850fb506d
yanashurpik <yana_shurpik@imap.by>
2020-05-08 23:36:18 +0300

    3 commit: checking login link

--- page/base_page.py
+++ page/base_page.py
@@ -1,9 +1,20 @@
-class BasePage():
+from selenium.common.exceptions import NoSuchElementException
+
+
+class BasePage:
 
     def __init__(self, browser, url):
         self.browser = browser
         self.url = url
+        self.browser.implicitly_wait(5)
 
     def open(self):
         self.browser.get(self.url)
+
+    def is_element_present(self, how, what):
+        try:
+            self.browser.find_element(how, what)
+        except NoSuchElementException:
+            return False
+        return True
 
