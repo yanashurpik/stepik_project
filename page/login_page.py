aa0fe0fbf91ccd1bdf7b1cb314624d72d5a850dc
yanashurpik <yana_shurpik@imap.by>
2020-05-10 21:28:32 +0300

    4 commit: testing login and register form

---
+++ page/login_page.py
@@ -0,0 +1,19 @@
+from .base_page import BasePage
+from .locators import LoginPageLocators
+
+
+class LoginPage(BasePage):
+    def should_be_login_page(self):
+        self.should_be_login_url()
+        self.should_be_login_form()
+        self.should_be_register_form()
+
+    def should_be_login_url(self):
+        login_url = self.browser.current_url
+        assert (login_url, "http://selenium1py.pythonanywhere.com/en-gb/accounts/login/"), "Login Url in not right"
+
+    def should_be_login_form(self):
+        assert self.is_element_present(*LoginPageLocators.LOGIN_FORM), "Login form is not presented"
+
+    def should_be_register_form(self):
+        assert self.is_element_present(*LoginPageLocators.REGISTER_FORM), "Register form is not presented"
--- test_main_page.py
+++ test_main_page.py
@@ -1,8 +1,14 @@
 from .page.main_page import MainPage
+from .page.login_page import LoginPage
 
 
 def test_guest_can_go_to_login_page(browser):
     link = "http://selenium1py.pythonanywhere.com/"
     page = MainPage(browser, link)   # инициализируем Page Object, передаем в конструктор экземпляр драйвера и url адрес
     page.open()                      # открываем страницу
-    page.should_be_login_link()
+    # page.should_be_login_link()
+    # login_page = page.go_to_login_page()  # 1 способ перехода поссылке
+
+    page.go_to_login_page()
+    login_page = LoginPage(browser, browser.current_url)
+    login_page.should_be_login_form()
